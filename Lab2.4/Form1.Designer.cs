4cfc18ba6d9f0495c349ee3d30107ff9a7d94c3c
Max Ilyin <max270398@gmail.com>
2017-11-29 01:09:46 +0300

    add lab 2.4

---
+++ Lab2.4/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DICTIONARY";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(551, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Полное имя без пробелов";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(340, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "ILYINMAKSY";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(697, 10);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(222, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "ILYINVASILENKOVOLODYM";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = " CRC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(183, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = " Bits";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(281, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Lun";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(376, 47);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = " EAN13";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(16, 94);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(903, 407);
+            this.textBox4.TabIndex = 11;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(471, 47);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(82, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "INN";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(561, 47);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(130, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = " RailwayTransport";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(697, 47);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(82, 23);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "CRC";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(785, 47);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(82, 23);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "ECC";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 513);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
---
+++ Lab2.4/Form1.cs
@@ -0,0 +1,478 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsoleRedirection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        TextWriter _writer = null;
+        string alphabet = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
+
+        string lastName = "ILYINMAKSY";
+
+        string full = "ILYINVASILENKOVOLODYM";
+
+
+        
+    public Form1()
+        {
+            InitializeComponent();
+            _writer = new TextBoxStreamWriter(textBox4);
+            Console.SetOut(_writer);
+
+            int[] positions = new int[full.Length];
+
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = alphabet.IndexOf(full[i]) + 1;
+            }
+
+
+
+
+
+           
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = " ";
+            CRC(new int[] { 23, 19, 17 });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = " ";            
+            Bits(lastName);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = " ";
+            int[] positions = new int[full.Length];
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = alphabet.IndexOf(full[i]) + 1;
+            }
+            Lun(MakeArray(positions, 15));
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = " ";
+            int[] positions = new int[full.Length];
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = alphabet.IndexOf(full[i]) + 1;
+            }
+            EAN13(MakeArray(positions, 13));
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = " ";
+            int[] positions = new int[full.Length];
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = alphabet.IndexOf(full[i]) + 1;
+            }
+            EAN13(MakeArray(positions, 13));
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = " ";
+            int[] positions = new int[full.Length];
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = alphabet.IndexOf(full[i]) + 1;
+            }
+            INN(MakeArray(positions, 10));
+        }
+        private void button7_Click(object sender, EventArgs e)
+
+        {
+            textBox4.Text = " ";
+            int[] positions = new int[full.Length];
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = alphabet.IndexOf(full[i]) + 1;
+            }
+            INN(MakeArray(positions, 10));
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            textBox4.Text = " ";
+            int[] positions = new int[full.Length];
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = alphabet.IndexOf(full[i]) + 1;
+            }
+            RailwayTransport(MakeArray(positions, 5));
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = " ";
+            int[] positions = new int[full.Length];
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = alphabet.IndexOf(full[i]) + 1;
+            }
+            CRC(new int[] { alphabet.IndexOf(lastName[0]) + 1, alphabet.IndexOf(lastName[1]) + 1, alphabet.IndexOf(lastName[2]) + 1 });
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = " ";
+            string message = lastName.Substring(0, 2);
+            byte[] messageInASCII = Encoding.ASCII.GetBytes(message);
+            string newMessage = "";
+
+
+            for (int i = 0; i < messageInASCII.Length; i++)
+            {
+                newMessage += Convert.ToString(messageInASCII[i], 2);
+            }
+
+            newMessage = newMessage.Substring(0, 11);
+
+            ECC(newMessage);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private static void ECC(string newMessage)
+        {
+            Console.WriteLine("ECC algorithm. Input");
+            Console.WriteLine(newMessage);
+
+            string result = "";
+
+            result = "11" + newMessage[0] + "0" + newMessage.Substring(1, 3) + "0" + newMessage.Substring(4, newMessage.Length - 4);
+
+            int pb = 0;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                pb += int.Parse(result[i].ToString());
+            }
+
+            pb %= 2;
+
+            Console.WriteLine(result + ", pb = " + pb);
+        }
+
+        private static void CRC(int[] v)
+        {
+            Console.WriteLine("G(x) = x^4 + x^1 + x^0");
+
+            Console.WriteLine("CRC algorithm. Input");
+
+            foreach (var item in v)
+            {
+                Console.Write(item + "\t");
+            }
+
+            Console.WriteLine();
+
+            string dect = "1011";
+
+            Console.WriteLine("dect = " + dect + "(" + Convert.ToInt32(dect, 2) + ")");
+
+            Console.WriteLine("Делимое \t *x^N \t Частное \t Остаток \t Результат");
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                string input = Convert.ToString(v[i], 2);
+
+                string input2 = string.Concat(input, "0000");
+
+                string chastnoe = GetChastnoeAndOstatokForCRC(input2, dect)[0];
+                string ostatok = GetChastnoeAndOstatokForCRC(input2, dect)[1];
+
+                int input2Integer = Convert.ToInt32(input2, 2);
+
+                int ostatokInteger = Convert.ToInt32(ostatok, 2);
+
+                int chastnoeInteger = Convert.ToInt32(chastnoe, 2);
+
+                string final = string.Concat(input, ostatok);
+
+                int finalRes = Convert.ToInt32(final, 2);
+
+                Console.WriteLine(v[i] + "(" + input + ")" + "\t" +
+                    input2 + "(" + input2Integer + ")" + "\t" +
+                   chastnoe + "(" + chastnoeInteger + ")" + "\t"
+                    + ostatok + "(" + ostatokInteger + ")" + "\t"
+                    + final + "(" + finalRes + ")");
+            }
+        }
+
+        private static string[] GetChastnoeAndOstatokForCRC(string imya2, string dect)
+        {
+            string delit, resultat = "";
+            bool f = true;
+            do
+            {
+                delit = "";
+                int l1 = imya2.Length - 1;
+                int l2 = dect.Length - 1;
+                int raz = l1 - l2;
+
+                string dect2 = dect;
+                for (int i = 0; i < raz; i++)
+                {
+                    dect2 = dect2 + "0";
+                }
+
+                for (int i = 0; i <= l1; i++)
+                {
+                    delit = delit + Convert.ToString(Convert.ToInt32(Convert.ToString(imya2[i]), 2) ^ Convert.ToInt32(Convert.ToString(dect2[i]), 2));
+                }
+
+                int counter = 0;
+
+                bool h = true;
+                if (delit.IndexOf('1') >= 0)
+                {
+                    do
+                    {
+                        if (delit[1] != '0')
+                            h = false;
+                        if (delit[0] == '0' && delit.Length >= dect.Length)
+                        {
+                            delit = delit.Substring(1, delit.Length - 1);
+                            counter++;
+                        }
+                        else
+                        {
+                            h = false;
+                        }
+                    }
+                    while (h);
+                }
+                else
+                {
+                    imya2 = "0000";
+                    resultat = "1" + new string('0', dect.Length - 1);
+                    f = false;
+                    break;
+                }
+
+                resultat += "1" + new string('0', counter - 1);
+
+
+                if (delit.Length < dect.Length)
+                    f = false;
+
+                imya2 = delit;
+            }
+            while (f);
+
+            return new string[] { resultat, imya2 };
+        }
+
+        private static void RailwayTransport(int[] v)
+        {
+            Console.WriteLine("RailwayTransport algorithm. Input");
+
+            foreach (var item in v)
+            {
+                Console.Write(item + "\t");
+            }
+
+            Console.WriteLine();
+
+            int controlNumber = (1 * v[0] + 2 * v[1] + 3 * v[2] + 4 * v[3]) % 11;
+
+            if (controlNumber == 10)
+            {
+                controlNumber = (3 * v[0] + 4 * v[1] + 5 * v[2] + 6 * v[3]) % 11;
+
+                if (controlNumber == 10)
+                {
+                    controlNumber = 0;
+                }
+            }
+
+            Console.WriteLine("LastNumber should be " + controlNumber);
+        }
+
+        private static void INN(int[] v)
+        {
+            Console.WriteLine("INN algorithm. Input");
+
+            foreach (var item in v)
+            {
+                Console.Write(item + "\t");
+            }
+
+            Console.WriteLine();
+
+            int result = ((2 * v[0] + 4 * v[1] + 10 * v[2] + 3 * v[3] + 5 * v[4] + 9 * v[5] + 5 * v[6] + 6 * v[7] + 8 * v[8]) % 11) % 10;
+
+            Console.WriteLine("LastNumber should be " + result);
+        }
+
+        private static void EAN13(int[] v)
+        {
+            Console.WriteLine("EAN-13 algorithm. Input");
+
+            foreach (var item in v)
+            {
+                Console.Write(item + "\t");
+            }
+
+            Console.WriteLine();
+
+            int sumNeChet = 0;
+            int sumChet = 0;
+
+            for (int i = 1; i <= v.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sumChet += v[i - 1];
+                }
+                else if (i != v.Length)
+                {
+                    sumNeChet += v[i - 1];
+                }
+            }
+
+            sumChet *= 3;
+
+            int result = 0;
+
+            for (int i = 0; ; i++)
+            {
+                if ((sumChet + sumNeChet + i) % 10 == 0)
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+
+            Console.WriteLine("LastNumber should be " + result);
+        }
+
+        private static void Lun(int[] v)
+        {
+            Console.WriteLine("Lun algorithm. Input");
+
+            foreach (var item in v)
+            {
+                Console.Write(item + "\t");
+            }
+
+            Console.WriteLine();
+
+            int sumNeChet = 0;
+            int sumChet = 0;
+
+            for (int i = 1; i <= v.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    v[i - 1] = (v[i - 1] * 2) % 9;
+
+                    sumChet += v[i - 1];
+                }
+                else
+                {
+                    sumNeChet += v[i - 1];
+                }
+            }
+
+            int result = 0;
+
+            for (int i = 0; ; i++)
+            {
+                if ((sumChet + sumNeChet + i) % 10 == 0)
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+
+            Console.WriteLine("LastNumber should be " + result);
+        }
+
+        private static void Bits(string lastName)
+        {
+            Console.WriteLine("Bits algorithm");
+            Console.WriteLine("Char \t Bits \t EvenBit \t OddBit");
+            foreach (char item in lastName)
+            {
+                byte[] messageInASCII = Encoding.ASCII.GetBytes(new char[] { item });
+                string newMessage = "";
+
+                for (int i = 0; i < messageInASCII.Length; i++)
+                {
+                    newMessage += Convert.ToString(messageInASCII[i], 2);
+                }
+
+                int EvenBit = (newMessage.Length - newMessage.Replace("1", "").Length) % 2;
+
+                int OddBit = EvenBit == 1 ? 0 : 1;
+
+                Console.WriteLine($"{item} \t {newMessage} \t {EvenBit} \t {OddBit}");
+            }
+
+        }
+
+        private static int[] MakeArray(int[] array, int count)
+        {
+            int[] result = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = array[i];
+            }
+
+            return result;
+        }
+
+
+    }
+}
